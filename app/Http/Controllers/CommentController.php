b2c8e6c000ec46b826395380b589ce75cbc963d8
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2018-09-12 17:05:55 +0200

    ajax number of comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -16,12 +16,14 @@ class CommentController extends Controller
             'user_id' => auth()->user()->id,
             'post_id' => request('id')
         ]);
-
+        
         return Comment::with('user')->where('post_id', request('id'))->get();
+        return Comment::with('post')->where('post_id', request('id'))->count();
     }
 
     public function show()
     {
         return Comment::with('user')->where('post_id', request('id'))->get();
     }
+
 }
