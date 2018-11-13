630b92952ed9f99ed438d67f279ae01f2d1e4970
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2018-11-13 18:26:38 +0100

    Profile page design

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -5,7 +5,36 @@
     @include('includes.header')
 
     <div class="container-fluid" style="background-color:#E6ECF0">
-        
+        <div class="row profile">
+            <div class="col-md-12 profile-info">
+                <p><b>{{ $user->name }} {{ $user->last_name }}</b></p>
+                <p>{{ $user->username }}</p>
+                <p>Joined {{ $user->created_at->format('F Y') }}</p>
+                @if (auth()->user()->id == $user->id)
+                    <button type="button" class="btn btn-primary editProfile" >Edit Profile</button>
+                @endif
+            </div>
+            <div class="col-md-6 offset-md-3 list"><br>
+                <table class="table">
+                    <thead>
+                        <tr align="center">
+                            <td>Posts</td>
+                            <td>Likes</td>
+                            <td>Followers</td>
+                            <td>Following</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr align="center">
+                            <td><b>{{ $user->posts->count() }}</b></td>
+                            <td><b>{{ $user->likes->count() }}</b></td>
+                            <td><b>5</b></td>
+                            <td><b>5</b></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
 @endsection
