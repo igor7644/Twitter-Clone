32c57c2d2acdcccd4cf2da8614e7e42dfa21b037
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2018-12-20 17:43:18 +0100

    Added method for follow and unfollow

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\EditProfileValidation;
+use Illuminate\Support\Facades\Auth;
 use App\Post;
 use App\User;
 
@@ -29,5 +30,17 @@ class UserController extends Controller
         $user->save();
 
         return User::find(request('id'));
+    }
+
+    public function follow($user)
+    {
+        Auth::user()->isFollowing()->attach($user);
+        return redirect()->back();
+    }
+
+    public function unfollow($user)
+    {
+        Auth::user()->isFollowing()->detach($user);
+        return redirect()->back();
     }
 }
