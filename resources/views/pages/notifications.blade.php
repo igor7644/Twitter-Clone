f032c2298e025636e44cf9a83cc9cec271eff8fe
Igor Milosavljević <igor.milosavljevic.72.14@ict.edu.rs>
2019-04-14 19:25:45 +0200

    Notifications blade

---
+++ resources/views/pages/notifications.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.layout')
+@section('title', 'Notifications | Social Blog')
+@section('body')
+
+    @include('includes.header')
+
+    <div class="container-fluid" style="background-color:#E6ECF0">
+        <div class="row">
+            <div class="col-md-6 offset-md-3 notifications">
+                <h3><b>Notifications</b></h3><br>
+                <form action="{{ asset('/markasread') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-primary markAsRead">Mark All As Read</button>
+                </form>
+                @foreach (Auth::user()->unReadNotifications as $notification)
+                    <div class="card notificationCard">
+                        <i class="far fa-bell" style="margin-top:20px;"></i>
+                        <div class="card-body">
+                            <p>{{ $notification->data['name'] }} {{ $notification->data['lastName'] }} (<a href="{{ asset('/user/'.$notification->data['username']) }}">{{ $notification->data['username'] }}</a>) started following you!</p>
+                        </div>
+                        <div class="card-footer">
+                        <small class="text-muted">
+                            <p>{{ $notification->created_at->diffForHumans() }}</p>
+                        </small>
+                        </div>
+                    </div>
+                @endforeach
+                @foreach (Auth::user()->readNotifications as $notification)
+                    <div class="card notificationCard readNotification">
+                        <i class="far fa-bell-slash" style="margin-top:20px;"></i>
+                        <div class="card-body">
+                            <p>{{ $notification->data['name'] }} {{ $notification->data['lastName'] }} (<a href="{{ asset('/user/'.$notification->data['username']) }}">{{ $notification->data['username'] }}</a>) started following you!</p>
+                        </div>
+                        <div class="card-footer">
+                        <small class="text-muted">
+                            <p>{{ $notification->created_at->diffForHumans() }}</p>
+                        </small>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            
+        </div>
+        
+    </div>
+
+@endsection
